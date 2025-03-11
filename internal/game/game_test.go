8588ec9e3ba4263da5857b5f4129ae83ce75baae
Avashist1998 vashist <abhay.vashist1998@gmail.com>
2025-03-11 22:12:52 +0000

    feat: testIsMoveValid

--- internal/game/game_test.go
+++ internal/game/game_test.go
@@ -16,9 +16,6 @@ Most Important (Core Game Logic & Rules)
         Determines if a player has won.
         A bug here could break the core game functionality.
 
-    isMoveValid(move int) bool
-        Prevents illegal moves from being made.
-
     updateGameWinner(index int)
         Ensures the correct player is marked as the winner.
 
@@ -33,14 +30,39 @@ func TestGameInitialization(t *testing.T) {
 	// Example test case
 
 	g := NewGame("player1", "player2")
-	
-	if g.Player1 != "player1"  {
-		t.Errorf("Expected %s, got %s", "player1" , g.Player1)
+
+	if g.Player1 != "player1" {
+		t.Errorf("Expected %s, got %s", "player1", g.Player1)
 	}
 
 	if g.Player2 != "player2" {
-		t.Errorf("Expected %s, got %s", "player2" , g.Player2)
+		t.Errorf("Expected %s, got %s", "player2", g.Player2)
 	}
 }
 
+func TestIsMoveValid(t *testing.T) {
 
+	g := NewGame("player1", "player2")
+	for i := range 10 {
+		if i < 7 {
+			if g.isMoveValid(i) == false {
+				t.Errorf("Expected move to be valid")
+			}
+		} else {
+			if g.isMoveValid(i) == true {
+				t.Errorf("Expected move to be invalid")
+			}
+		}
+	}
+
+	for i := range 6 {
+		if g.isMoveValid(0) == false {
+			t.Errorf("Expected move to be valid for %d th time", i)
+		}
+		g.makeMove(0, g.GetCurrSlot())
+	}
+	if g.isMoveValid(0) == true {
+		t.Errorf("Expected move to be invalid")
+	}
+
+}
